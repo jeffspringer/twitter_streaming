113b0cc13c6fd0e8edc81c2dec0ec2850b7401c0
Jeff Springer <jeffspringer3000@gmail.com>
2019-04-02 10:39:18 -0600

    Add files via upload

---
+++ accessConfig.py
@@ -0,0 +1,6 @@
+#Twitter app credentials
+
+consumer_key = 'HtXN3EvLH6Pclnuuw9KKwqjVv'
+consumer_secret = 'hiBBe6AP3iD3bYXCBRb7sYgpfdMvSzbDb1HtEbO1rdoTvAnWMi'
+access_token = '2387937997-Zuhai7H4N9aQnrNZdFNDy73UJkQ8wHay218k79i'
+access_secret = 'hiBBe6AP3iD3bYXCBRb7sYgpfdMvSzbDb1HtEbO1rdoTvAnWMi'
