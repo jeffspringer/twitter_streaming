0a44dac25754e53cda04219538e0ee2b603abc49
Jeff Springer <jeffspringer3000@gmail.com>
2019-04-02 11:56:46 -0600

    Update getBatchData.py

--- getBatchData.py
+++ getBatchData.py
@@ -10,6 +10,8 @@ import time
 auth = OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_secret)
 
+print(consumer_key)
+
 api = tweepy.API(auth)
 queryHashtag = 'DonaldTrump'
 
