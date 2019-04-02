76814997decd05e6c32850e8a26fec2972386d6c
Jeff Springer <jeffspringer3000@gmail.com>
2019-04-02 12:50:21 -0600

    Delete accessConfig.py

--- accessConfig.py
+++
@@ -1,6 +0,0 @@
-#Twitter app credentials
-
-consumer_key = 'b4eHIwVDsQDgXTSpdSvYVkBji'
-consumer_secret = 'Mxb3QUpcYC5uN0BdIzX8Mk46x4UvpztXS9NOjMCC7rNLZQEMZP'
-access_token = '2387937997-yeSsk9645tuRv48WqciXnfDqT7i2UaB2HpSdw0V'
-access_secret = 'Btz2PHRn4lGfRhiN47VxbQJq3iDRd2P7pkIOxKtzsrVyh'
