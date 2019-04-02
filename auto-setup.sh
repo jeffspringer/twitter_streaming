2849acbfbc52eea0091431967359d36735277464
Jeff Springer <jeffspringer3000@gmail.com>
2019-04-02 11:33:28 -0600

    Update auto-setup.sh

--- auto-setup.sh
+++ auto-setup.sh
@@ -5,4 +5,4 @@ sudo apt-get install python
 sudo apt-get install python-setuptools python-dev build-essential
 sudo easy_install pip
 sudo pip install pip-requirements.txt
-nohup python getBatchData.py &
+#nohup python getBatchData.py &
